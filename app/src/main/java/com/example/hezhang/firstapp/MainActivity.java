1d3ebdf1282195022c3daef99bad834742d9e0ff
zhang.he <zhang.he@wenba100.com>
2017-10-27 18:20:32 +0800

    fragment-tab

--- app/src/main/java/com/example/hezhang/firstapp/MainActivity.java
+++ app/src/main/java/com/example/hezhang/firstapp/MainActivity.java
@@ -1,18 +1,118 @@
 package com.example.hezhang.firstapp;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.graphics.Color;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    /**
+     * 用于展示消息的Fragment
+     */
+    private MessageFragment messageFragment;
+
+    /**
+     * 用于展示联系人的Fragment
+     */
+    private ContactsFragment contactsFragment;
+
+    /**
+     * 用于展示动态的Fragment
+     */
+    private NewsFragment newsFragment;
+
+    /**
+     * 用于展示设置的Fragment
+     */
+    private SettingFragment settingFragment;
+
+    /**
+     * 消息界面布局
+     */
+    private View messageLayout;
+
+    /**
+     * 联系人界面布局
+     */
+    private View contactsLayout;
+
+    /**
+     * 动态界面布局
+     */
+    private View newsLayout;
+
+    /**
+     * 设置界面布局
+     */
+    private View settingLayout;
+
+    /**
+     * 在Tab布局上显示消息图标的控件
+     */
+    private ImageView messageImage;
+
+    /**
+     * 在Tab布局上显示联系人图标的控件
+     */
+    private ImageView contactsImage;
+
+    /**
+     * 在Tab布局上显示动态图标的控件
+     */
+    private ImageView newsImage;
+
+    /**
+     * 在Tab布局上显示设置图标的控件
+     */
+    private ImageView settingImage;
+
+    /**
+     * 在Tab布局上显示消息标题的控件
+     */
+    private TextView messageText;
+
+    /**
+     * 在Tab布局上显示联系人标题的控件
+     */
+    private TextView contactsText;
+
+    /**
+     * 在Tab布局上显示动态标题的控件
+     */
+    private TextView newsText;
+
+    /**
+     * 在Tab布局上显示设置标题的控件
+     */
+    private TextView settingText;
+
+    /**
+     * 用于对Fragment进行管理
+     */
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_main);
+
+        // 初始化布局元素
+        initViews();
+        fragmentManager = getFragmentManager();
+
+        setTabSelection(0);
 
 //        Display display = getWindowManager().getDefaultDisplay();
 //        Point point = new Point();
@@ -84,4 +184,146 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    /**
+     * 在这里获取到每个需要用到的控件的实例，并给它们设置好必要的点击事件。
+     */
+    private void initViews() {
+        messageLayout = findViewById(R.id.message_layout);
+        contactsLayout = findViewById(R.id.contacts_layout);
+        newsLayout = findViewById(R.id.news_layout);
+        settingLayout = findViewById(R.id.setting_layout);
+
+        messageImage = (ImageView) findViewById(R.id.message_image);
+        contactsImage = (ImageView) findViewById(R.id.contacts_image);
+        newsImage = (ImageView) findViewById(R.id.news_image);
+        settingImage = (ImageView) findViewById(R.id.setting_image);
+
+        messageText = (TextView) findViewById(R.id.message_text);
+        contactsText = (TextView) findViewById(R.id.contacts_text);
+        newsText = (TextView) findViewById(R.id.news_text);
+        settingText = (TextView) findViewById(R.id.setting_text);
+
+        messageLayout.setOnClickListener(this);
+        contactsLayout.setOnClickListener(this);
+        newsLayout.setOnClickListener(this);
+        settingLayout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.message_layout:
+                // 当点击了消息tab时，选中第1个tab
+                setTabSelection(0);
+                break;
+            case R.id.contacts_layout:
+                // 当点击了联系人tab时，选中第2个tab
+                setTabSelection(1);
+                break;
+            case R.id.news_layout:
+                // 当点击了动态tab时，选中第3个tab
+                setTabSelection(2);
+                break;
+            case R.id.setting_layout:
+                // 当点击了设置tab时，选中第4个tab
+                setTabSelection(3);
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * 根据传入的index参数来设置选中的tab页。
+     *
+     * @param index
+     *            每个tab页对应的下标。0表示消息，1表示联系人，2表示动态，3表示设置。
+     */
+    private void setTabSelection(int index) {
+        clearSelection();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        hideFragments(transaction);
+        switch (index) {
+            case 0:
+                messageImage.setImageResource(R.drawable.comments_selected);
+                messageText.setTextColor(Color.WHITE);
+                if( messageFragment == null ) {
+                    messageFragment = new MessageFragment();
+                    transaction.add(R.id.content, messageFragment);
+                } else {
+                    transaction.show(messageFragment);
+                }
+                break;
+            case 1:
+                contactsImage.setImageResource(R.drawable.account_selected);
+                contactsText.setTextColor(Color.WHITE);
+                if( contactsFragment == null ) {
+                    contactsFragment = new ContactsFragment();
+                    transaction.add(R.id.content, contactsFragment);
+                } else {
+                    transaction.show(contactsFragment);
+                }
+                break;
+            case 2:
+                newsImage.setImageResource(R.drawable.favorite_selected);
+                newsText.setTextColor(Color.WHITE);
+                if( newsFragment == null ) {
+                    newsFragment = new NewsFragment();
+                    transaction.add(R.id.content, newsFragment);
+                } else {
+                    transaction.show(newsFragment);
+                }
+                break;
+            case 3:
+            default:
+                settingImage.setImageResource(R.drawable.set_selected);
+                settingText.setTextColor(Color.WHITE);
+                if( settingFragment == null ) {
+                    settingFragment = new SettingFragment();
+                    transaction.add(R.id.content, settingFragment);
+                } else {
+                    transaction.show(settingFragment);
+                }
+                break;
+        }
+        transaction.commit();
+    }
+
+    /**
+     * 清除掉所有的选中状态。
+     */
+    private void clearSelection() {
+        messageImage.setImageResource(R.drawable.comments);
+        messageText.setTextColor(Color.parseColor("#82858b"));
+        contactsImage.setImageResource(R.drawable.account);
+        contactsText.setTextColor(Color.parseColor("#82858b"));
+        newsImage.setImageResource(R.drawable.favorite);
+        newsText.setTextColor(Color.parseColor("#82858b"));
+        settingImage.setImageResource(R.drawable.set);
+        settingText.setTextColor(Color.parseColor("#82858b"));
+    }
+
+
+    /**
+     * 将所有的Fragment都置为隐藏状态。
+     *
+     * @param transaction
+     *            用于对Fragment执行操作的事务
+     */
+    private void hideFragments(FragmentTransaction transaction) {
+        if (messageFragment != null) {
+            transaction.hide(messageFragment);
+        }
+        if (contactsFragment != null) {
+            transaction.hide(contactsFragment);
+        }
+        if (newsFragment != null) {
+            transaction.hide(newsFragment);
+        }
+        if (settingFragment != null) {
+            transaction.hide(settingFragment);
+        }
+    }
+
 }
